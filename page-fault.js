b7b63cdf59406e845152922a65359d221dcb0e75
Ben N <bentinata@gmail.com>
2017-05-08 22:31:02 +0700

    Strip codes.

--- page-fault.js
+++ page-fault.js
@@ -1,27 +1,11 @@
 'use strict';
 
-if (process.env.LOG)
-  var log = console.log
-else
-  var log = () => {}
-
-
 function countmap(page, future) {
   const count = page.map((value, index) => {
     return future.reduce((acc, curr) => acc += curr == value ? 1 : 0, 0)
   })
 
   return count.indexOf(Math.min(...count))
-}
-
-function optimalmap(page, future) {
-  const count = page.map((value, index) => {
-    return future.indexOf(value)
-  })
-
-  //const max =  count.indexOf(Math.max(...count))
-  //log(`map: ${max} ${count}`)
-  return count.indexOf(Math.max(...count))
 }
 
 function optimal(seq, start) {
@@ -31,7 +15,7 @@ function optimal(seq, start) {
   for (let i = mem.length; i < seq.length; i += 1) {
     if (i === 0 && !mem.length) {
       mem.push([seq[i]])
-      continue;
+      continue
     }
 
     const current = mem[i - 1].slice()
@@ -40,27 +24,18 @@ function optimal(seq, start) {
       if (current.length < 4) {
         current.push(seq[i])
       } else {
-        let index = optimalmap(current, seq.slice(i + 1))
+        const future = seq.slice(i + 1)
+        const count = current.map((value, index) => {
+          return future.indexOf(value)
+        })
+
+        const index = count.indexOf(Math.max(...count))
         current[index] = seq[i]
       }
-    } else {
     }
 
     mem.push(current)
   }
+
   return mem;
 }
-
-function print(seq, interval = 200) {
-  let i = 1;
-  const clock = setInterval(() => {
-    const page = seq.shift()
-    log(i, page)
-    if (!seq.length) clearInterval(clock)
-    i += 1;
-  }, interval)
-}
-
-//log(optimalmap(['a', 'b', 'c', 'd'], Array.from('babcd')))
-const opt = optimal(Array.from('cadbebabcd'))
-print(opt)
